ed011c970902bc6f56ef94720dab2c90704936ab
Anna Berger <anna.ig.berger@gmail.com>
2022-06-07 00:09:41 +0300

    fix minor things: inputs, python version

--- run_DV_estimation.py
+++ run_DV_estimation.py
@@ -11,13 +11,15 @@ from src.io import format_output, load
 OUTPUT_FOLDER = Path("./data/outputs/")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Linear approximation algorithm.")
+    parser = argparse.ArgumentParser("Domain analysis algorithm.")
+    parser.add_argument("--db_file", type=str, required=True)
     parser.add_argument("--num_categs", type=int, required=True)
     parser.add_argument("--n_angles", type=int, required=True)
     parser.add_argument("--output_folder", type=Path, default=OUTPUT_FOLDER)
     args = parser.parse_args()
 
-    raw_data = load("./data/ESPGameNN.txt")
+    raw_data = load(args.db_file)
+    db_name = Path(args.db_file).stem
     data = List()
     [data.append(x) for x in raw_data]
 
@@ -27,7 +29,7 @@ if __name__ == "__main__":
 
     np.savetxt(
         OUTPUT_FOLDER
-        / (f"python_ESPGameNN_{args.num_categs}_{args.n_angles}.txt"),
+        / (f"python_{db_name}_{args.num_categs}_{args.n_angles}.txt"),
         hull,
         delimiter=" ",
     )
